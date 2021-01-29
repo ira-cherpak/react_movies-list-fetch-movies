5d48b03aca1477f97538f424b810226a6773e122
Iryna Cherpak <cherpak.iryna@gmail.com>
2021-01-29 10:07:47 +0200

    Updates

---
+++ precache-manifest.6bac38a4e449f83fbb17e4568fdc1564.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68fcb3dd8bb170a4bbcf25de7546e8ca",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ac32e0f6dd45a0cea50f",
+    "url": "./static/css/2.9634ee28.chunk.css"
+  },
+  {
+    "revision": "9abf2efe91027883dcef",
+    "url": "./static/css/main.6ce43aae.chunk.css"
+  },
+  {
+    "revision": "ac32e0f6dd45a0cea50f",
+    "url": "./static/js/2.6c23e112.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "./static/js/2.6c23e112.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9abf2efe91027883dcef",
+    "url": "./static/js/main.6b88605b.chunk.js"
+  },
+  {
+    "revision": "aae0386031766cdb5520",
+    "url": "./static/js/runtime-main.63136dd4.js"
+  }
+]);
